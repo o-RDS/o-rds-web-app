6e75e8c22c3281ef9e37e49a715c7448908d4ada
Morgan Jackson <mbj4school@gmail.com>
2022-10-20 13:18:45 -0500

    react default cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import { HashRouter, Routes, Route } from "react-router-dom";
 import LoginAdmin from "./pages/LoginAdmin";
 import DashboardAdmin from "./pages/DashboardAdmin";
