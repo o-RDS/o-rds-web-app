f79a8764b96e65f42495539518dfaac2a09e9943
Cwoodard2 <cawooda@siue.edu>
2022-11-06 16:19:43 -0600

    Added pages for payment manager, results and survey builder

---
+++ src/pages/admin/PaymentManagerAdmin.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import StandardPage from "../../components/StandardPage";
+
+export default function PaymentManager() {
+    return (
+        <StandardPage>
+            <p>This is the payment manager</p>
+        </StandardPage>
+    )
+}
---
+++ src/pages/admin/SurveyBuilderAdmin.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import StandardPage from "../../components/StandardPage";
+
+export default function SurveyBuilder() {
+    return (
+        <StandardPage>
+            <p>This is the survey builder</p>
+        </StandardPage>
+    )
+}
