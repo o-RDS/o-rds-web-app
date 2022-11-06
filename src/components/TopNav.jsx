ddfa29a809095588b69ee160b6a8f225edf845e3
Cwoodard2 <cawooda@siue.edu>
2022-11-05 19:36:26 -0500

    Small update to navbar

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function TopNav() {
     return (
-        <div className="flex flex-row border-b-4">
-            <div>
-                <h3 className="text-lg bg-gradient-to-br from-emerald-700 to-orange-600 inline-block text-transparent bg-clip-text">o-RDS</h3>
+        <div className="flex flex-row border-b-4 w-screen justify-end">
+            <h3 className="text-lg bg-gradient-to-br from-emerald-700 to-orange-600 inline-block text-transparent bg-clip-text justify-self-start">o-RDS</h3>
+            <div className="flex justify-self-end items-center gap-2">
+                <p>Home</p>
+                <p>Payments</p>
+                <p>Help</p>
+                <p>Profile</p>
             </div>
-            <p>Home</p>
-            <p>Payments</p>
-            <p>Help</p>
-            <p>Profile</p>
         </div>
     );
 }
