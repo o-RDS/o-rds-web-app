218ce8eaf73dc1ff700c43be96f05eb8dc3df333
Morgan Jackson <morjack@siue.edu>
2022-11-12 01:10:34 -0600

    add functions for passing data up component tree

--- src/components/questions/Question.tsx
+++ src/components/questions/Question.tsx
@@ -1,24 +1,43 @@
 import React from "react";
 import MultipleChoice from "./MultipleChoice";
+import "./Question.css";
 
 export default function Question(props: any) {
-  // I may move all configurable properties into an object for easier passing and typing
+  // this defines the properties that should be present on each question
   interface QuestionData {
     type: string;
     page: number;
     config?: Object;
   };
 
+  // this will retrieve the questions default config, add type and page, and pass it to the SurveyBuilder component (up the chain)
+  function getConfig(configData: Object) {
+    const data: QuestionData = {
+      type: props.data.type,
+      page: props.data.page,
+      config: configData,
+    };
+    // TODO call a function passed from SurveyBuilder to update the config
+  }
+
+  // this will be called in the question, then called a function from Survey to pass new response data up the chain
+  function updateResponse(answers: Object) {
+    if (props.handleResponse) {
+      props.handleResponse(answers, props.index);
+    }
+  }
+
   // This function will take the type property of the question object and return the appropriate component, passing the needed data
   function getQuestionType(data: QuestionData) {
+    console.log(data);
     switch (data.type) {
       case "MultipleChoice":
-        return <MultipleChoice data={data.config}/>;
+        return <MultipleChoice config={data.config} updateResponse={updateResponse}/>;
       default:
         return <p>"Unknown Question Type"</p>;
     }
   }
 
   // Returns the appropriate component
-  return <div>{getQuestionType(props.data)}</div>;
+  return <div className="Question">{getQuestionType(props.data)}</div>;
 }
