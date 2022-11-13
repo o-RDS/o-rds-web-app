70dfed0be4deff7d46d8b3778c1113fca9f83473
Kevin Belobrajdic <kebelob@siue.edu>
2022-11-13 10:16:10 -0600

    Implemented retrieveSurveyData()

--- src/data/dataLayerManager.ts
+++ src/data/dataLayerManager.ts
@@ -1,11 +1,40 @@
+import { initializeApp } from "firebase/app"
+import {
+    getFirestore, doc, getDoc
+} from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6d-taZfAJEpjzl46igYQjx0l-l2Xp92E",
+  authDomain: "o-rds-edcc9.firebaseapp.com",
+  projectId: "o-rds-edcc9",
+  storageBucket: "o-rds-edcc9.appspot.com",
+  messagingSenderId: "467120561765",
+  appId: "1:467120561765:web:e08ecab4e2e6998740708c"
+};
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
 export function writeSurveyResponse() {
     return (1);
 }
 
-export function retrieveSurveyData(id: number) {
-
+export async function retrieveSurveyData(id: string) {
+    const db = getFirestore();
+    const docRef = doc(db, "surveys", id);
+    try {
+        const docSnap = await getDoc(docRef);
+        if(docSnap.exists()) {
+            return docSnap.data();
+        } else {
+            console.log("Document does not exist")
+        }
+    
+    } catch(error) {
+        console.log(error)
+    }
 }
 
 export function createSurvey() {
-
+     
 }
