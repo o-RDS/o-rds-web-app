60c89c4f7fa3e05c829b731cf1dc675c02f9b913
Cwoodard2 <cawooda@siue.edu>
2023-01-13 18:31:34 -0600

    Basic UI for survey builder. Still needs a lot more work

--- src/pages/admin/SurveyBuilderAdmin.tsx
+++ src/pages/admin/SurveyBuilderAdmin.tsx
@@ -4,7 +4,39 @@ import StandardPage from "../../components/StandardPage";
 export default function SurveyBuilder() {
     return (
         <StandardPage>
-            <p>This is the survey builder</p>
+            <div className="flex flex-row gap-20">
+                <div className="flex flex-col justify-start border-r border-black w-1/5">
+                    <p>words</p>
+                </div>
+                <div className="flex flex-col w-3/5 h-min-56 gap-10 mt-3">
+                    <div className="flex flex-row justify-between">
+                        <input placeholder="Survey Name" className="bg-gray-100 text-white rounded-md"></input>
+                        <div className="flex gap-2">
+                            <button className="border-rdsBlue border text-rdsBlue rounded-sm pl-2 pr-2">Preview</button>
+                            <button className="bg-rdsBlue text-white rounded-sm pl-2 pr-2">Publish</button>
+                        </div>
+                    </div>
+                    <div className="border-black border justify-center items-center flex flex-col p-5 rounded-md gap-10 w-full">
+                        <div className="w-full">
+                            <h3>Q1</h3>
+                            <div className="bg-gray-100 p-3 rounded-md">
+                                <input placeholder="Click to Write Question Text"></input>
+                                <ul>
+                                    <li><input type="radio" value="Item 1"></input><label>Item 1</label></li>
+                                    <li><input type="radio" placeholder="Item 1"></input><label>Item 2</label></li>
+                                    <li><input type="radio" placeholder="Item 1"></input><label>Item 3</label></li>
+                                    <li><input type="radio" placeholder="Item 1"></input><label>Item 4</label></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <button className="bg-rdsBlue text-white rounded-sm pl-2 pr-2 w-fit">+ Add Question</button>
+                        <div className="w-full">
+                            <h3>End of Survey</h3>
+                            <input className="w-full bg-gray-100" placeholder="Put what you would like the end of survey message to be!"></input>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </StandardPage>
     )
 }
