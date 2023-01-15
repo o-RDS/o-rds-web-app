00c3004f22d52337332cd7bbe2c16b838d799f4c
Cwoodard2 <cawooda@siue.edu>
2023-01-14 19:40:39 -0600

    Changes to update UI some

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -5,7 +5,7 @@ import helpIcon from '../images/help_icon.png';
 export default function TopNav() {
     return (
         <div className="flex flex-row border-b-4 w-screen justify-start h-14 items-center px-2">
-            <h3 className="text-lg bg-gradient-to-br from-emerald-700 to-orange-600 inline-block text-transparent bg-clip-text justify-start items-center">o-RDS</h3>
+            <h3 className="text-lg bg-gradient-to-br from-rdsBlue to-rdsOrange inline-block text-transparent bg-clip-text justify-start items-center">o-RDS</h3>
             <div className="flex items-center gap-2 ml-auto">
                 {/* <p>Home</p> */}
                 <Link to="/DashboardAdmin">
