34e0f2c4bd0134006910dc711b7058f7573a0a75
Morgan Jackson <morjack@siue.edu>
2023-01-21 20:55:28 -0600

    prepped for sms, redirect if no phone

--- src/pages/user/OTPCodeEntry.tsx
+++ src/pages/user/OTPCodeEntry.tsx
@@ -1,36 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import SurveyTakerStandardPage from "../../components/SurveyTakerStandardPage";
 
 export default function OTPCodeEntry() {
-    function verifyOTPCode(code: string) {
-        // TODO Verify code, allow user to continue to survey
-    }
+  const navigate = useNavigate();
+  const [code, setCode] = useState("");
+  const [error, setError] = useState("");
 
-    function displayPhone() {
-        let num = window.sessionStorage.getItem('phone')
-        if (num != null) {
-            return "(" + num.slice(0, 3) + ") " + num.slice(3, 6) + "-" + num.slice(6, 10)
-        } else {
-            return "Invalid Phone Number"
-        }
+  function verifyOTPCode() {
+    if (code.length !== 6) {
+      setError("Invalid Code");
+      return;
+    } else {
+      // TODO Verify code, allow user to continue to survey
     }
+  }
 
-    return(
-        <SurveyTakerStandardPage>
-            <div className="flex flex-col">
-                <p>
-                Your code has been sent to {displayPhone()}. <br /><br />
-                Please enter it in the field below.
-                </p>
-            </div>
-            <div className="flex flex-col gap-y-6">
-                <div className="flex flex-col">
-                    <label htmlFor="OTPCode">Code:<br /></label>
-                    <input type="text" id="OTPCode" name="OTPCode"  placeholder="*Code Format Here*" className="w-56 p-1 rounded bg-gray-200"></input>
-                </div>
-                <button className="p-1 w-56 rounded bg-rdsOrange text-white">Submit</button>
-            </div>
-        </SurveyTakerStandardPage>
-    )
+  useEffect(() => {
+    if (window.sessionStorage.getItem("phone") == null) {
+      navigate("..");
+    }
+  }, []);
+
+  function displayPhone() {
+    let num = window.sessionStorage.getItem("phone");
+    if (num != null) {
+      return (
+        "(" + num.slice(0, 3) + ") " + num.slice(3, 6) + "-" + num.slice(6, 10)
+      );
+    } else {
+      console.log("You shouldn't be here! (No Phone Number Found)");
+      return "Invalid Phone Number";
+    }
+  }
+
+  return (
+    <SurveyTakerStandardPage>
+      <div className="flex flex-col">
+        <p>
+          Your code has been sent to {displayPhone()}. <br />
+          <br />
+          Please enter it in the field below.
+        </p>
+      </div>
+      <div className="flex flex-col gap-y-6">
+        <div className="flex flex-col">
+          <label htmlFor="OTPCode">
+            Code:
+            <br />
+          </label>
+          <input
+            type="text"
+            id="OTPCode"
+            name="OTPCode"
+            placeholder="*Code Format Here*"
+            className="w-56 p-1 rounded bg-gray-200"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          ></input>
+        </div>
+        <button
+          className="p-1 w-56 rounded bg-rdsOrange text-white"
+          onClick={() => verifyOTPCode()}
+        >
+          Submit
+        </button>
+        <p className="text-red-600">{error}</p>
+      </div>
+    </SurveyTakerStandardPage>
+  );
 }
