1e4317d90e23637a099348c70dfe23d83e5c9ee5
Morgan Jackson <morjack@siue.edu>
2023-01-21 20:51:25 -0600

    updates to how sessionStorage is handled

--- src/data/sessionManager.ts
+++ src/data/sessionManager.ts
@@ -1,13 +1,15 @@
+import Sha256 from "./Sha256";
+
 export function setPhone(phoneNumber: string) {
   window.sessionStorage.setItem('phone', phoneNumber);
-  window.sessionStorage.setItem('hash', hashGen(phoneNumber));
+  window.sessionStorage.setItem('hash', Sha256.hash(phoneNumber));
+  console.log('Phone number set to: ' + phoneNumber);
+  console.log('Hash set to: ' + Sha256.hash(phoneNumber));
 };
 
-export function setSurveyID(surveyID: string) {
-  window.sessionStorage.setItem('surveyID', surveyID);
-};
-
-function hashGen(phoneNumber: string) {
-  // TODO: Implement hashGen
-  return phoneNumber;
+export function setChainInfo(parent: string, depth: number) {
+  window.sessionStorage.setItem('parent', parent);
+  window.sessionStorage.setItem('depth', depth.toString());
+  console.log('Parent set to: ' + parent);
+  console.log('Depth set to: ' + depth);
 }
