4d7e5eabef9585c90169149703309dd14687d029
Cwoodard2 <cawooda@siue.edu>
2023-01-25 23:13:52 -0600

    Added components within sidebar to deal with updating + rendering correcgt options

---
+++ src/components/sidebar/MCConfigs.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+export default function MCConfig(props: any) {
+    const choicesArray: any = props.config.config.choices.value.map((choice: any) => <li key={choice}>{choice}</li>);
+    const dealWithChange = (e: any) => {
+        // console.log(e.target.value);
+        let test: any = props.config;
+        test["config"]["prompt"]["value"] = e.target.value;
+        console.log(test);
+        console.log(test["config"]["prompt"]["value"]);
+        props.updateQuestion(test);
+      }
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <label>
+          {
+            props.config.config.prompt.configPrompt
+          }
+        </label>
+        <input
+          type="text"
+          placeholder="This is a question"
+          className="w-3/5 rounded-sm border border-rdsOrange"
+          onChange={(e: any) => dealWithChange(e)}
+        ></input>
+      </div>
+      <div>
+        <label>
+          {
+            props.config.config.shuffle.configPrompt
+          }
+        </label>
+        <input type="checkbox"></input>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <label>
+          {
+            props.config.config.choices.configPrompt
+          }
+        </label>
+        <input
+          type="text"
+          placeholder="Add Choices Here"
+          className="w-3/5 rounded-sm border border-rdsOrange"
+        ></input>
+        <ul>{choicesArray}</ul>
+      </div>
+    </>
+  );
+}
