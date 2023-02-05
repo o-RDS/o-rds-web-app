4ae8b25354e52f72963e40b0794125a8040123c8
Morgan Jackson <morjack@siue.edu>
2023-02-05 01:53:33 -0600

    smoother reload w/ sessionStorage

--- src/context/ConfigContext.tsx
+++ src/context/ConfigContext.tsx
@@ -1,30 +1,42 @@
-import react, { useEffect, useRef } from 'react';
-import { useParams, useNavigate, Outlet } from 'react-router';
-import { retrieveSurveyConfig, signIn } from '../data/dataLayerManager';
+import react, { useEffect, useRef } from "react";
+import { useParams, useNavigate, Outlet } from "react-router";
+import { retrieveSurveyConfig, signIn } from "../data/dataLayerManager";
 
-
-export default function ConfigContext(props: any){
+export default function ConfigContext(props: any) {
   //const SurveyConfigContext = react.createContext({});
   const navigate = useNavigate();
   const params = useParams();
-  const config = useRef({});
+  const config = useRef(
+    window.sessionStorage.getItem(
+      params.id && typeof params.id === "string" ? params.id : ""
+    )
+      ? JSON.parse(
+          window.sessionStorage.getItem(
+            params.id && typeof params.id === "string" ? params.id : ""
+          ) as string
+        )
+      : {}
+  );
 
   useEffect(() => {
     signIn();
-    if(params.id){
-      console.log(params.id)
+    if (params.id && typeof params.id === "string") {
+      console.log(params.id);
       retrieveSurveyConfig(params.id).then((data) => {
         if (data == null) {
-          navigate("/invalid")
+          navigate("/invalid");
         } else {
-          console.log(data)
+          console.log(data);
           config.current = data;
+          if (params.id && typeof params.id === "string") {
+            window.sessionStorage.setItem(params.id, JSON.stringify(data));
+          }
         }
-      })
+      });
     } else {
-      navigate("/invalid")
+      navigate("/invalid");
     }
-  }, [])
+  }, []);
 
-  return (<Outlet context={config.current}/>)
+  return <Outlet context={config.current} />;
 }
