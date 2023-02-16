90b48b4ba4830791a93054713648e75ebbfa69c5
Morgan Jackson <morjack@siue.edu>
2023-02-15 22:11:30 -0600

    pages for viewing surveys on dashboard

--- src/pages/admin/DashboardAdmin.tsx
+++ src/pages/admin/DashboardAdmin.tsx
@@ -4,15 +4,21 @@ import { loadAdminSurveys } from "../../data/dataLayerManager";
 import { useState, useEffect } from "react";
 
 export default function Dashboard() {
-  const user = "test"
+  const user = "test";
   const [surveys, setSurveys] = useState([{ id: 1, title: "Loading..." }]);
+  const [page, setPage] = useState(0);
+  const [displayNumber, setDisplayNumber] = useState(5);
 
   useEffect(() => {
-    loadAdminSurveys(user).then((surveys) => {
-      console.log(surveys)
+    loadAdminSurveys(
+      user,
+      page * displayNumber,
+      (page + 1) * displayNumber
+    ).then((surveys) => {
+      console.log(surveys);
       setSurveys(surveys);
     });
-  }, []);
+  }, [page, displayNumber]);
 
   function renderSurveyButtons() {
     return surveys.map((survey) => (
@@ -32,8 +38,19 @@ export default function Dashboard() {
       <div className="flex flex-col items-center">
         <h1>Welcome User</h1>
         <div className="flex w-5/6 flex-col">
-          <div className="flex flex-row">
-            <h3 className="text-2xl">Active Surveys</h3>
+          <div className="flex flex-row items-baseline gap-2">
+            <h3 className="text-2xl">My Surveys</h3>
+            <p className="pl-5">Surveys Per Page:</p>
+            <button onClick={() => setDisplayNumber(5)} className={`${displayNumber === 5 ? "text-rdsOrange" : ""}`}>5</button>
+            <button onClick={() => setDisplayNumber(10)} className={`${displayNumber === 10 ? "text-rdsOrange" : ""}`}>10</button>
+            <button onClick={() => setDisplayNumber(25)}  className={`pr-5 ${displayNumber === 25 ? "text-rdsOrange" : ""}`}>25</button>
+            {page > 0 ? (
+              <button onClick={() => setPage(page - 1)}>{"<"}</button>
+            ) : (
+              <button disabled>{"<"}</button>
+            )}
+            <p>Page {page + 1}</p>
+            <button onClick={() => setPage(page + 1)}>{">"}</button>
             <div className="ml-auto">
               <Link to="../survey-builder/new">
                 <button className="rounded-md bg-rdsBlue p-1 pl-2 pr-2 text-white">
