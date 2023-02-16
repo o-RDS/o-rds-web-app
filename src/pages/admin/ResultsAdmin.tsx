0d3bc6294ee86c2a76b37011361b1eb0a3de4cf2
Morgan Jackson <morjack@siue.edu>
2023-02-16 01:42:15 -0600

    added loading + removed dummy data

--- src/pages/admin/ResultsAdmin.tsx
+++ src/pages/admin/ResultsAdmin.tsx
@@ -1,102 +1,12 @@
 import { useState, useEffect } from "react";
 import ResultRow from "../../components/ResultRow";
 import StandardPage from "../../components/StandardPage";
-import { loadAllResponses, retrieveSurveyConfig } from "../../data/dataLayerManager";
+import {
+  loadAllResponses,
+  retrieveSurveyConfig,
+} from "../../data/dataLayerManager";
 import { useParams, useNavigate } from "react-router-dom";
-
-const dummySurveyConfig = {
-  question1: {
-    prompt: {
-      value: "Wie geht's?",
-    },
-  },
-  question2: {
-    prompt: {
-      value: "Which is the capital of France?",
-    },
-  },
-  question3: {
-    prompt: {
-      value:
-        "What is the name for the German ice cream dish that imitates an Italian pasta dish?",
-    },
-  },
-  question4: {
-    prompt: {
-      value:
-        "What is the name for the German ice cream dish that imitates an Italian pasta dish?",
-    },
-  },
-  question5: {
-    prompt: {
-      value:
-        "What is the name for the German ice cream dish that imitates an Italian pasta dish?",
-    },
-  },
-  question6: {
-    prompt: {
-      value:
-        "What is the name for the German ice cream dish that imitates an Italian pasta dish?",
-    },
-  },
-  question7: {
-    prompt: {
-      value:
-        "What is the name for the German ice cream dish that imitates an Italian pasta dish?",
-    },
-  },
-  question8: {
-    prompt: {
-      value:
-        "What is the name for the German ice cream dish that imitates an Italian pasta dish?",
-    },
-  },
-  question9: {
-    prompt: {
-      value:
-        "What is the name for the German ice cream dish that imitates an Italian pasta dish?",
-    },
-  },
-  question10: {
-    prompt: {
-      value:
-        "What is the name for the German ice cream dish that imitates an Italian pasta dish?",
-    },
-  },
-  question11: {
-    prompt: {
-      value:
-        "What is the name for the German ice cream dish that imitates an Italian pasta dish?",
-    },
-  },
-};
-
-const dummySurveyResponses = {
-  1234: {
-    answers: {
-      Q1: "Ich bin gut!",
-      Q2: "A",
-      Q3: "Spaghettieis",
-      Q4: "Ich bin gut!",
-      Q5: "A",
-      Q6: "Spaghettieis",
-      Q7: "Ich bin gut!",
-      Q8: "A",
-      Q9: "Spaghettieis",
-      Q10: "Ich bin gut!",
-      Q11: "A",
-    },
-    completed: true,
-  },
-  5432: {
-    answers: {
-      Q1: "Ich bin müde...",
-      Q2: "C",
-      Q3: "Spaghettieis SpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieisSpaghettieis",
-    },
-    completed: false,
-  },
-};
+import Loading from "../../components/Loading";
 
 export default function Results() {
   const [results, setResults] = useState<any>(null);
@@ -106,7 +16,7 @@ export default function Results() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (params.surveyID){
+    if (params.surveyID) {
       loadAllResponses(params.surveyID).then((results) => {
         setResults(results);
       });
@@ -114,8 +24,8 @@ export default function Results() {
         setConfig(config);
       });
     } else {
-      console.log("No survey ID provided")
-      navigate("/admin/dashboard")
+      console.log("No survey ID provided");
+      navigate("/admin/dashboard");
     }
   }, []);
 
@@ -128,7 +38,7 @@ export default function Results() {
         let currUserResponses = [];
         currUserResponses.push(responses[userID].responseID);
         for (let questionID in responses[userID].answers) {
-          currUserResponses.push(responses[userID].answers[questionID]); 
+          currUserResponses.push(responses[userID].answers[questionID]);
         }
         currUserResponses.push(responses[userID].completed.toString()); //Store completed status at end of array
         allUserResponses.push(currUserResponses);
@@ -139,10 +49,10 @@ export default function Results() {
   }
 
   function renderTableHeader() {
-    let questions:Array<any> = config.questions;
+    let questions: Array<any> = config.questions;
     let headers = [];
 
-    headers.push("User ID"); //NOTE THAT THIS VALUE IS CURRENTLY HARD-CODED IN. THIS CAN PROBABLY BE CHANGED IN THE FUTURE
+    headers.push("User ID");
     questions.forEach((question) => {
       headers.push(question.config.prompt.value);
     });
@@ -185,11 +95,14 @@ export default function Results() {
           </button>
         </div>
         <div className="overflow-auto">
-          {results && config &&
-          <table className="mb-4 w-fit table-auto border-collapse text-left">
-            <thead>{renderTableHeader()}</thead>
-            <tbody>{renderTableBody(filterCompleted)}</tbody>
-          </table>}
+          {results && config ? (
+            <table className="mb-4 w-fit table-auto border-collapse text-left">
+              <thead>{renderTableHeader()}</thead>
+              <tbody>{renderTableBody(filterCompleted)}</tbody>
+            </table>
+          ) : (
+            <Loading />
+          )}
         </div>
       </div>
     </StandardPage>
