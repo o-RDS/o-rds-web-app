f38b6b214a17bf2233a2008dc1867e4a7dd2f859
Kevin Belobrajdic <kebelob@siue.edu>
2023-02-19 09:08:31 -0600

    Updated Tremendous function to match server

--- src/APIs/Tremendous.js
+++ src/APIs/Tremendous.js
@@ -82,8 +82,7 @@ export async function sendPayment(order, JWT) {
         email: order.recipient_email,
         phone: order.recipient_phone
       },
-      method: order.delivery_method,
-      to: order.recipient_phone
+      method: order.delivery_method
     })
   };
 
