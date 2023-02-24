fb09c59f9d8bac87e8f5832ec7082ac92ef5a5fe
Morgan Jackson <morjack@siue.edu>
2023-02-24 17:19:23 -0600

    more API calls implemented

--- src/APIs/Firebase.js
+++ src/APIs/Firebase.js
@@ -10,9 +10,9 @@ else if (process.env.NODE_ENV == "production") {
 }
 
 function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
 // SURVEY FUNCTIONS
@@ -20,7 +20,7 @@ function getCookie(name) {
 // GET /api/survey
 export function retrieveSurveyConfig(id) {
   const token = "JWT " + getCookie("token");
-  
+
   const options = {
     method: 'GET',
     headers: {
@@ -35,18 +35,18 @@ export function retrieveSurveyConfig(id) {
 
   var statusCode;
   return fetch(`${serverHost}/api/survey`, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    statusCode = response.status;
-    return response.json()
-  })
-  .then((data) => {
-    data.statusCode = statusCode;
-    console.log(data);
-    return data;
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
 // GET /api/surveys
@@ -68,18 +68,18 @@ export function loadAdminSurveys(index, limit) {
 
   var statusCode;
   return fetch(`${serverHost}/api/surveys`, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    statusCode = response.status;
-    return response.json()
-  })
-  .then((data) => {
-    data.statusCode = statusCode;
-    console.log(data);
-    return data;
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
 // POST /api/survey
@@ -101,18 +101,18 @@ export function saveSurveyConfig(surveyID, surveyData) {
 
   var statusCode;
   return fetch(`${serverHost}/api/survey`, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    statusCode = response.status;
-    return response.json()
-  })
-  .then((data) => {
-    data.statusCode = statusCode;
-    console.log(data);
-    return data;
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
 // DELETE /api/survey
@@ -133,18 +133,18 @@ export function deleteSurveyConfig(surveyID) {
 
   var statusCode;
   return fetch(`${serverHost}/api/survey`, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    statusCode = response.status;
-    return response.json()
-  })
-  .then((data) => {
-    data.statusCode = statusCode;
-    console.log(data);
-    return data;
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
 // RESPONSE FUNCTIONS
@@ -168,18 +168,18 @@ export function loadResponse(surveyID, alias) {
 
   var statusCode;
   return fetch(`${serverHost}/api/response`, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    statusCode = response.status;
-    return response.json()
-  })
-  .then((data) => {
-    data.statusCode = statusCode;
-    console.log(data);
-    return data;
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
 // GET /api/responses
@@ -200,18 +200,18 @@ export function loadAllResponses(surveyID) {
 
   var statusCode;
   return fetch(`${serverHost}/api/responses`, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    statusCode = response.status;
-    return response.json()
-  })
-  .then((data) => {
-    data.statusCode = statusCode;
-    console.log(data);
-    return data;
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
 // POST /api/response
@@ -234,57 +234,199 @@ export async function writeSurveyResponse(surveyID, alias, response) {
 
   var statusCode;
   return fetch(`${serverHost}/api/response`, options)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText)
-    }
-    statusCode = response.status;
-    return response.json()
-  })
-  .then((data) => {
-    data.statusCode = statusCode;
-    console.log(data);
-    return data;
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
 // TODO: DELETE /api/response 
 
 // INCENTIVE FUNCTIONS
 
-// POST /api/hash
-export function addHash(surveyID, hash) {
-  
+// POST /api/incentive
+export function addHash(surveyID) {
+  const token = "JWT " + getCookie("token");
+
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      Authorization: token
+    },
+    body: JSON.stringify({
+      surveyID: surveyID,
+    })
+  };
+
+  var statusCode;
+  return fetch(`${serverHost}/api/incentive`, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
+/* DEPRECATED, WILL AUTOMATICALLY HANDLE WHEN SAVING COMPLETE RESPONSE
 // POST /api/incentive
 export function completeIncentive(surveyID, hash) {
   
 }
+*/
 
 // GET /api/incentive
-export function loadIncentiveInfo(surveyID, hash) {
-  
+export function loadIncentiveInfo(surveyID) {
+  const token = "JWT " + getCookie("token");
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      Authorization: token
+    },
+    body: JSON.stringify({
+      surveyID: surveyID,
+    })
+  };
+
+  var statusCode;
+  return fetch(`${serverHost}/api/incentive`, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
+/* TODO
 // PUT /api/incentive
 export async function updateIncentiveInfo(surveyID, hash, data) {
   
 }
+*/
 
 // OTHER FUNCTIONS
 
 // POST /api/alias
 export function generateAlias(surveyID) {
-  
+  const token = "JWT " + getCookie("token");
+
+  const options = {
+    method: 'POST',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      Authorization: token
+    },
+    body: JSON.stringify({
+      surveyID: surveyID,
+    })
+  };
+
+  var statusCode;
+  return fetch(`${serverHost}/api/response`, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
 // PATCH /api/user-remove
-export function removeSurveyFromUser(userId, surveyID) {
-  
+export function removeSurveyFromUser(userID, surveyID) {
+  const token = "JWT " + getCookie("token");
+
+  const options = {
+    method: 'PATCH',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      Authorization: token
+    },
+    body: JSON.stringify({
+      surveyID: surveyID,
+      userID: userID
+    })
+  };
+
+  var statusCode;
+  return fetch(`${serverHost}/api/user-remove`, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
 }
 
 // PATCH /spi/user-add
 export function addSurveyToUser(userID, surveyID) {
-  
+  const token = "JWT " + getCookie("token");
+
+  const options = {
+    method: 'PATCH',
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      Authorization: token
+    },
+    body: JSON.stringify({
+      surveyID: surveyID,
+      userID: userID
+    })
+  };
+
+  var statusCode;
+  return fetch(`${serverHost}/api/user-add`, options)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      statusCode = response.status;
+      return response.json()
+    })
+    .then((data) => {
+      data.statusCode = statusCode;
+      console.log(data);
+      return data;
+    });
+
 }
