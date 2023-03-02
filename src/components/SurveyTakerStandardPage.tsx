1649d32664ea99461107eda9bd5775253491fadc
Zach Sappington <zach.sappington@gmail.com>
2023-03-02 15:20:37 -0600

    Contact Modal UI updated and now pulls from config

--- src/components/SurveyTakerStandardPage.tsx
+++ src/components/SurveyTakerStandardPage.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import sampleResearcherLogo from "../images/sample_researcher_logo.png";
-import contactResearcherIcon from "../images/contact_researcher_icon.png";
+import contactResearcherIcon from "../images/contact_researcher.svg"
 import ContactMenuModal from "./ContactMenuModal";
 
 export default function SurveyTakerStandardPage(props: any) {
@@ -8,19 +8,19 @@ export default function SurveyTakerStandardPage(props: any) {
 
   return (
     <>
-      <div className="flex h-screen w-screen flex-col items-center gap-y-3 p-6">
+      <div className="flex h-screen w-screen flex-col items-center gap-y-3">
         <ContactMenuModal 
           display={displayModal}
           setDisplay={setDisplayModal}
         />
-        <div className="justify-start self-start">
+        <div className="justify-start self-start p-2">
           <button
             onClick={() => setDisplayModal(!displayModal)}
           >
             <img src={contactResearcherIcon}/>
           </button>
         </div>
-        <div className="flex h-full w-full flex-col items-center gap-y-8">
+        <div className="flex h-full w-full flex-col items-center gap-y-8 px-6">
           <img src={sampleResearcherLogo} className="w-3/4 md:w-1/3 lg:w-1/4" />
           {props.children}
         </div>
