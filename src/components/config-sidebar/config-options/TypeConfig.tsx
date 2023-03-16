a6dd4441e13b0a56e8ad2b8ac5055422d1746db9
Zach Sappington <zach.sappington@gmail.com>
2023-03-15 21:49:53 -0500

    Fixed Question Type dark mode dropdown

--- src/components/config-sidebar/config-options/TypeConfig.tsx
+++ src/components/config-sidebar/config-options/TypeConfig.tsx
@@ -21,12 +21,12 @@ export default function TypeConfig() {
     <div className="flex flex-col gap-2">
       <label>Question Type</label>
       <select
-        className="rounded-md border border-rdsBlue p-2 dark:bg-inherit dark:text-white"
+        className="rounded-md border border-rdsBlue p-2 dark:bg-rdsDarkAccent3 dark:text-white"
         onChange={(e) => handleTypeChange(e)}
       >
         <option>Select An Option</option>
         <option>Multiple Choice</option>
-        <option>FillInBlank</option>
+        <option>Fill-in-the-Blank</option>
         <option>Short Answer</option>
         <option>Checkbox</option>
       </select>
