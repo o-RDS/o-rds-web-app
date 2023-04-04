9278378207826e8fb7cd4d9b2bd3270c5462f090
Morgan Jackson <mbj4school@gmail.com>
2023-04-03 21:56:45 -0500

    added state for loading campaigns

--- src/components/settings/TremendousSettings.tsx
+++ src/components/settings/TremendousSettings.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SurveyContext, SurveyDispatchContext } from "../../context/SurveyBuilderContext";
 import { listCampaigns, listFundingSources } from "../../APIs/Tremendous";
 
@@ -24,63 +24,89 @@ interface fundingSource {
   availableCents: number;
 }
 
-function loadCampaignData() {
+async function loadCampaignData() {
   let campaigns: campaign[] = [];
 
-  listCampaigns().then((data) => {
-    data.campaigns.forEach((obj: any) => {
-      var tempCampaign: campaign = {
-        name: obj.name,
-        id: obj.id,
-        products: obj.products,
-      };
-      campaigns.push(tempCampaign);
-    });
+  let data = await listCampaigns()
+  data.campaigns.forEach((obj: any) => {
+    var tempCampaign: campaign = {
+      name: obj.name,
+      id: obj.id,
+      products: obj.products,
+    };
+    campaigns.push(tempCampaign);
   });
 
   return campaigns;
 }
 
-function loadFundingData() {
+async function loadFundingData() {
   let fundingSources: fundingSource[] = [];
 
-  listFundingSources().then((data) => {
-    data.funding_sources.forEach((obj: any) => {
-      var tempFundingSource: fundingSource = {
-        method: obj.method,
-        id: obj.id,
-        availableCents: obj.meta.available_cents,
-      };
-      fundingSources.push(tempFundingSource);
-    });
+  let data = await listFundingSources()
+  data.funding_sources.forEach((obj: any) => {
+    var tempFundingSource: fundingSource = {
+      method: obj.method,
+      id: obj.id,
+      availableCents: obj.meta.available_cents,
+    };
+    fundingSources.push(tempFundingSource);
   });
 
   return fundingSources;
 }
 
 export function TremendousSettings() {
-    const dispatch = useContext(SurveyDispatchContext);
+  const [campaigns, setCampaigns] = useState<campaign[]>([]);
+  const [fundingSources, setFundingSources] = useState<fundingSource[]>([]);
+  const dispatch = useContext(SurveyDispatchContext);
+
   function handleCampaignChange(e: any) {
-    let type = e.target.value;
+    e.preventDefault();
+    let name = e.target.value;
+    let id = "";
+    campaigns.forEach((campaign: any) => {
+      if (campaign.name === name) {
+        id = campaign.id;
+      }
+    });
+    if (id === "") {
+      console.error("Funding source not found");
+      return;
+    }
     dispatch({
       type: "change-campaign",
-      newType: type
+      newType: id
     });
   }
 
   function handleFundingSourceChange(e: any) {
-    let type = e.target.value;
+    e.preventDefault();
+    let method = e.target.value;
+    let id = "";
+    fundingSources.forEach((source: any) => {
+      if (source.method === method) {
+        id = source.id;
+      }
+    });
+    if (id === "") {
+      console.error("Funding source not found");
+      return;
+    }
     dispatch({
       type: "change-funding",
-      newType: type
+      newType: id
     });
   }
 
-  let campaigns: campaign[] = loadCampaignData();
-  console.log(campaigns);
+  useEffect(() => {
+    loadCampaignData().then((data) => setCampaigns(data));
+    console.log(campaigns);
 
-  let fundingSources: fundingSource[] = loadFundingData();
-  console.log(fundingSources);
+    loadFundingData().then((data) => setFundingSources(data));
+    console.log(fundingSources);
+  }, []);
+
 
   return (
     <div className="flex w-full flex-col gap-10 pl-2 pr-2">
