679053ccefaba1f837bfc6248c1cf1dee6bd7eac
Cwoodard2 <cawooda@siue.edu>
2023-04-13 22:28:29 -0500

    Small change to appeance settings

--- src/components/settings/AppearanceSettings.tsx
+++ src/components/settings/AppearanceSettings.tsx
@@ -4,8 +4,28 @@ export function AppearanceSettings() {
     return (
         <div className="flex flex-col gap-10 w-full pl-2 pr-2">
             <h3 className="text-3xl">Appearance</h3>
-            <p>Main Color</p>
-            <p>Accent Color</p>
+            <div className="flex flex-col gap-1">
+        <label htmlFor="main-color">Main Color</label>
+        <input
+          type="number"
+          id="main-color"
+          className="w-2/5 rounded-sm p-1 dark:bg-rdsDarkAccent bg-gray-200"
+          maxLength={20}
+        //   onChange={(e) => handlePaidRefChange(e)}
+        //   value={SurveyState['survey']['maxRefIncentives']}
+        ></input>
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="accent-color">Accent Color</label>
+        <input
+          type="number"
+          id="accent-color"
+          className="w-2/5 rounded-sm p-1 dark:bg-rdsDarkAccent bg-gray-200"
+          maxLength={20}
+        //   onChange={(e) => handleMaxRefChange(e)}
+        //   value={SurveyState['survey']['maxRefs']}
+        ></input>
+      </div>
           </div>
     )
 }
